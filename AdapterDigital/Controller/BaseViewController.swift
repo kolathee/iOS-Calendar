3f7bfdd1332b01c9e3ca680ebff97a536df9ea6f
Kolathee Payuhawattana <kolathee@icloud.com>
2019-10-21 00:17:27 +0700

    Create BaseViewController

---
+++ AdapterDigital/Controller/BaseViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BaseViewController.swift
+//  AdapterDigital
+//
+//  Created by Kolathee Payuhawatthana on 20/10/19.
+//  Copyright © 2019 Kolathee Payuhawatthana. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    /// Overide these properties in subclasses
+    var identifier: String { "" }
+    var storyboardName: String { "" }
+    // static let shared = ViewController()
+    
+// MARK: - Setup
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+// MARK: - Functions
+    
+    func initiateView() -> UIViewController {
+        let storyBoard = UIStoryboard(name: storyboardName, bundle: nil)
+        return storyBoard.instantiateViewController(identifier: identifier)
+    }
+    
+}
